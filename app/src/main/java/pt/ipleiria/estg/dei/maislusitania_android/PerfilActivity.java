825d24c3a0d58ca7d2671bb5631bfe88c2a05354
MartimMendesIPL <2241559@my.ipleiria.pt>
2026-01-04 16:49:13 +0000

    editar perfil utilizador, mudar pass, apagar perfil(soft delete))

--- app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/PerfilActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/PerfilActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -37,6 +41,91 @@ public class PerfilActivity extends AppCompatActivity {
                     .replace(android.R.id.content, fragment)
                     .addToBackStack(null)
                     .commit();
+        });
+
+        binding.layoutEditarPerfil.setOnClickListener(v -> {
+            // Abrir caixa de diálogo para editar perfil
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+
+            View dialogView = inflater.inflate(R.layout.dialog_edit_profile, null);
+
+            EditText etFirstName = dialogView.findViewById(R.id.tvFirstName);
+            EditText etLastName = dialogView.findViewById(R.id.tvLastName);
+            EditText etUsername = dialogView.findViewById(R.id.tvUsername);
+
+            etFirstName.setText(binding.tvFirstName.getText().toString());
+            etLastName.setText(binding.tvLastName.getText().toString());
+            etUsername.setText(binding.tvUsername.getText().toString());
+
+            builder.setView(dialogView)
+                    .setTitle("Editar Perfil")
+                    .setPositiveButton("Guardar", (dialog, which) -> {
+                        String firstName = etFirstName.getText().toString().trim();
+                        String lastName = etLastName.getText().toString().trim();
+                        String username = etUsername.getText().toString().trim();
+
+                        if (!firstName.isEmpty() && !lastName.isEmpty() && !username.isEmpty()) {
+                            SingletonLusitania.getInstance(this).editUserProfileAPI(
+                                    this,
+                                    firstName,
+                                    lastName,
+                                    username
+                            );
+                        } else {
+                            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        });
+
+        binding.layoutMudarPassword.setOnClickListener(v -> {
+            // Abrir caixa de diálogo para mudar password
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+
+            View dialogView = inflater.inflate(R.layout.dialog_change_password, null);
+
+            EditText etCurrentPassword = dialogView.findViewById(R.id.etCurrentPassword);
+            EditText etNewPassword = dialogView.findViewById(R.id.etNewPassword);
+
+            builder.setView(dialogView)
+                    .setTitle("Mudar Password")
+                    .setPositiveButton("Guardar", (dialog, which) -> {
+                        String currentPassword = etCurrentPassword.getText().toString().trim();
+                        String newPassword = etNewPassword.getText().toString().trim();
+
+                        if (!currentPassword.isEmpty() && !newPassword.isEmpty()) {
+                            SingletonLusitania.getInstance(this).changePasswordAPI(
+                                    this,
+                                    currentPassword,
+                                    newPassword
+                            );
+                            Toast.makeText(this, "Password alterada com sucesso", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        });
+        // Botão Apagar Conta
+        binding.layoutApagarConta.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Remover Conta")
+                    .setMessage("Tem a certeza que pretende remover a sua conta? Esta ação é irreversível.")
+                    .setPositiveButton("Sim", (dialog, which) -> {
+                        SingletonLusitania.getInstance(this).deleteUserAPI(this);
+                        //Sair da conta
+                        binding.layoutLogout.performClick();
+                        Toast.makeText(this, "Conta removida com sucesso", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("Não", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
         });
 
         // Botão Logout
@@ -82,6 +171,9 @@ public class PerfilActivity extends AppCompatActivity {
 
             @Override
             public void onPerfilLogoutError(String error) {}
+
+            @Override
+            public void onPasswordChanged() {}
         });
 
         SingletonLusitania.getInstance(this).getUserProfileAPI(this);
--- app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/utils/UserJsonParser.java
+++ app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/utils/UserJsonParser.java
@@ -4,50 +4,41 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import androidx.annotation.NonNull;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 import pt.ipleiria.estg.dei.maislusitania_android.models.User;
 
 public class UserJsonParser {
-    // 1. Método para converter um Objeto JSON num Objeto Java (user)
+
     public static User parserJsonUser(JSONArray response) {
         User auxuser = null;
         try {
-            //Verificar se o array não está vazio
             if (response.length() == 0) {
-                android.util.Log.e("UserJsonParser", "Array vazio");
                 return null;
             }
 
-            //Pegar o primeiro objeto do array
             JSONObject user = response.getJSONObject(0);
 
-            int id = user.getInt("id");
-            String primeiroNome = user.getString("primeiro_nome");
-            String ultimoNome = user.getString("ultimo_nome");
-            String imagemPerfil = user.optString("imagem_perfil", ""); // ✅ Usar optString para null
-            int userId = user.getInt("user_id");
-            String username = user.getString("username");
-            String email = user.getString("email");
-            String dataAdesao = user.getString("data_adesao");
+            int id = user.optInt("id", -1);
+            String primeiroNome = user.optString("primeiro_nome", "Sem Nome");
+            String ultimoNome = user.optString("ultimo_nome", "");
+            String imagemPerfil = user.optString("imagem_perfil", "");
+
+            int userId = user.optInt("user_id", -1);
+            String username = user.optString("username", "");
+            String email = user.optString("email", "");
+            String dataAdesao = user.optString("data_adesao", "");
 
             auxuser = new User(id, primeiroNome, ultimoNome, imagemPerfil, userId, username, email, dataAdesao);
 
         } catch (JSONException e) {
-            android.util.Log.e("UserJsonParser", "Erro ao parsear JSON: " + e.getMessage());
             e.printStackTrace();
         }
         return auxuser;
     }
 
-
-    // 2. Método para verificar a Internet
     public static boolean isConnectionInternet(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
