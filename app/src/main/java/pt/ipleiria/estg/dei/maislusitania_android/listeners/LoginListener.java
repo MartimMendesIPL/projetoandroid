287cc21bf8aeadd90cac2fb1f2cabef53e1864f0
Henrik Dambros <2241589@my.ipleiria.pt>
2026-01-11 16:16:30 +0000

    adicionado funcionalidade de registo

--- app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/listeners/LoginListener.java
+++ app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/listeners/LoginListener.java
@@ -2,5 +2,4 @@ package pt.ipleiria.estg.dei.maislusitania_android.listeners;
 
 public interface LoginListener {
     void onValidateLogin(String authKey, String userName, String user_id);
-
 }
---
+++ app/src/main/java/pt/ipleiria/estg/dei/maislusitania_android/listeners/SignupListener.java
@@ -0,0 +1,5 @@
+package pt.ipleiria.estg.dei.maislusitania_android.listeners;
+
+public interface SignupListener {
+    void onSignupSuccess();
+}
